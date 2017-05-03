013e060e4785711f3011d1f634ddcf25f72e11f2
pareekrachit <pareekrachit@gmail.com>
2017-05-03 18:56:16 +0530

    Update pisa_model.R

--- Linear Regression/pisa_model.R
+++ Linear Regression/pisa_model.R
@@ -1,12 +1,18 @@
-#Average test score of male and female
-tapply(pisatrain$readingScore, pisatrain$male, mean)
+#Load training and testing datasets
+pisaTrain <- read.csv("C:/Users/532771/Documents/The Analytics Edge/Week 2/Reading Test Score/pisa2009train.csv")
+pisaTest <- read.csv("C:/Users/532771/Documents/The Analytics Edge/Week 2/Reading Test Score/pisa2009test.csv")
 
-summary(pisatrain)
+#Average test score of male and female
+summary(pisaTrain)
+tapply(pisaTrain$readingScore, pisaTrain$male, mean)
 
 #Removing missing data from dataset
-pisaTrain1 = na.omit(pisatrain)
-pisaTest1 = na.omit(pisatest)
+pisaTrain1 = na.omit(pisaTrain)
+pisaTest1 = na.omit(pisaTest)
 
 
 str(pisaTrain1)
 table(pisaTrain1$raceeth)
+levels(pisaTrain1$raceeth)
+
+plot(pisaTrain1$raceeth)
