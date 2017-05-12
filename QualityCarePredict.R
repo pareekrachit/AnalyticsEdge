221ded82be8580d5ac8804ac5cfb7df8452cf4c9
pareekrachit <pareekrachit@gmail.com>
2017-05-12 20:38:52 +0530

    Delete QualityCarePredict.R

--- QualityCarePredict.R
+++
@@ -1,8 +0,0 @@
-#Predict using QualityCareModel on training set
-predictQualityCare <- predict(QualityCareModel, type = "response")
-
-summary(predictQualityCare)
-
-tapply(predictQualityCare, qualityTrain$PoorCare, mean)
-
-
