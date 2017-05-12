769c6a8a1538b52719f8c98c3f2cd41aadad511d
pareekrachit <pareekrachit@gmail.com>
2017-05-12 20:38:20 +0530

    pareekrachit/AnalyticsEdge/LogisticModel

---
+++ QualityCareModel.R
@@ -0,0 +1,26 @@
+#Load data
+quality <- read.csv("~/The Analytics Edge/Week 3/Modeling the Expert/quality.csv")
+str(quality)
+
+table(quality$PoorCare)
+
+plot(quality$OfficeVisits, quality$Narcotics, col = ifelse(quality$PoorCare == 1, "red", "green"), pch = 20)
+
+#Install package caTools
+install.packages("caTools")
+library(caTools)
+
+#Split data set
+set.seed(88)
+split = sample.split(quality$PoorCare, SplitRatio = 0.75)
+split
+table(split)
+
+qualityTrain <- subset(quality, split == TRUE)
+qualityTest <- subset(quality, split == FALSE)
+
+table(qualityTrain$PoorCare)
+table(qualityTest$PoorCare)
+
+QualityCareModel <- glm(PoorCare ~ OfficeVisits + Narcotics, data = qualityTrain, family = binomial)
+summary(QualityCareModel)
