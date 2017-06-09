badef079ec9cb62c547a8c6f1bd02c2d65b3bbfe
pareekrachit <pareekrachit@gmail.com>
2017-06-09 20:02:33 +0530

    Add files via upload

--- Logistic Model/SongsModel.R
+++ Logistic Model/SongsModel.R
@@ -15,3 +15,27 @@ table(songs$timesignature)
 which.max(songs$tempo)
 songs$songtitle[6206]
 
+#Create training and test dataset
+SongsTrain <- subset(songs, year <= 2009)
+SongsTest <- subset(songs, year > 2009)
+
+#Drop unnecessary variables
+drop <- c("year", "songtitle", "artistname", "songID", "artistID")
+SongsTrain <- SongsTrain[ , !(names(SongsTrain) %in% drop)]
+SongsTest <- SongsTest[ , !(names(SongsTest) %in% drop)]
+
+#Create model1
+SongsModel1 <- glm(Top10 ~ ., data = SongsTrain, family = binomial)
+
+summary(SongsModel1)
+
+#Correlation
+cor(SongsTrain$loudness, SongsTrain$energy)
+
+#Create model2, remove loudness(multicollinearity)
+SongsModel2 <- glm(Top10 ~ .-loudness, data = SongsTrain, family = binomial)
+summary(SongsModel2)
+
+#Create model3, remove energy(multicollinearity)
+SongsModel3 <- glm(Top10 ~ .-energy, data = SongsTrain, family = binomial)
+summary(SongsModel3)
